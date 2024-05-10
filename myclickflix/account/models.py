6dbb40b138d516e467ba0d2c9b8575d20046097d
trungviet17 <113108053+trungviet17@users.noreply.github.com>
2024-05-10 22:32:59 +0700

    update reset_password feature

--- myclickflix/account/models.py
+++ myclickflix/account/models.py
@@ -8,5 +8,9 @@ class Profile(models.Model) :
                                 on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
 
+    address = models.TextField(null = True)
+    
+    phone = models.CharField(max_length=10, null = True)
+
     def __str__(self) : 
         return f'Profile of {self.user.username}'

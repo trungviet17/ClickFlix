beaeffa1787a885ff656093aebd835f65eebacb2
Tuoc Nguyen <nguyenductuoc862@gmail.com>
2024-05-13 00:23:20 +0700

    update UI

--- myclickflix/account/urls.py
+++ myclickflix/account/urls.py
@@ -5,8 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')), 
-    # path('reset_password/', auth_views.PasswordResetView.as_view(), name = 'reset_password'),
-    # path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name = 'password_reset_done'),
     path('logout' , auth_views.LogoutView.as_view(), name = 'logout'),
     path('register/', views.register, name = 'register'), 
     path('edit/', views.edit, name='edit'), 

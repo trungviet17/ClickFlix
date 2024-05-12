8db1abfd5527e24fa1a2976ed2faf50f551574d2
trungviet17 <113108053+trungviet17@users.noreply.github.com>
2024-05-12 17:09:59 +0700

    add new link into password_reset html

--- myclickflix/account/urls.py
+++ myclickflix/account/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')), 
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name = 'reset_password'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name = 'password_reset_done'),
+    # path('reset_password/', auth_views.PasswordResetView.as_view(), name = 'reset_password'),
+    # path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name = 'password_reset_done'),
     path('logout' , auth_views.LogoutView.as_view(), name = 'logout'),
     path('register/', views.register, name = 'register'), 
     path('edit/', views.edit, name='edit'), 

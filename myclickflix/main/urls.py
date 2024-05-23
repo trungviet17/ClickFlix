2eac89be35c907887a75f7ec38b57a6b90be930c
quangster <buiduyquang.5904@gmail.com>
2024-05-23 18:08:15 +0700

    Update details view, adminsite
    movies, categories, actors

---
+++ myclickflix/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from main.views import movie_detail, actor_detail, category_detail
+
+
+urlpatterns = [
+    path("movie/<slug:movie_slug>/", movie_detail, name="movie_detail"),
+    path("actor/<slug:actor_slug>/", actor_detail, name="actor_detail"),
+    path("category/<slug:category_slug>", category_detail, name="category_detail"),
+]
--- myclickflix/main/views.py
+++ myclickflix/main/views.py
@@ -1,3 +1,53 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from main.models import Category, Movie, Actor
 
-# Create your views here.
+
+def movie_detail(request, movie_slug):
+    try:
+        movie = Movie.objects.get(slug=movie_slug)
+    except Movie.DoesNotExist:
+        return HttpResponseNotFound("Movie not found")
+    actors = movie.actors.all()
+    categories = movie.categories.all()
+
+    context = {
+        "movie": movie,
+        "actors": actors,
+        "categories": categories,
+    }
+    return render(request, "product/movie.html", context=context)
+
+
+def actor_detail(request, actor_slug):
+    try:
+        actor = Actor.objects.get(slug=actor_slug)
+    except Movie.DoesNotExist:
+        return HttpResponseNotFound("Actor not found")
+    movies = actor.movie.all()
+
+    context = {
+        "actor": actor,
+        "movies": movies,
+    }
+    return render(request, "product/actor.html", context=context)
+
+
+def category_detail(request, category_slug):
+    try:
+        category = Category.objects.get(slug=category_slug)
+    except Category.DoesNotExist:
+        return HttpResponseNotFound("Category not found")
+
+    movies = category.movie.all()
+    paginator = Paginator(movies, 25)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "category": category,
+        "movies": page_obj,
+    }
+    return render(request, "product/category.html", context=context)

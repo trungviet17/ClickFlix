7b88ddd4a014e393da0b3aca3eace770c4042891
quangster <buiduyquang.5904@gmail.com>
2024-05-25 22:38:33 +0700

    fix

--- myclickflix/main/urls.py
+++ myclickflix/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from main.views import (
     movie_detail,
     actor_detail,
-    move_list,
+    search_movie,
     get_category,
     your_movie_list,
 )
@@ -10,7 +10,7 @@ from main.views import (
 
 urlpatterns = [
     path("mymovie/", your_movie_list, name="my_movie_list"),
-    path("search/", move_list, name="search_movie"),
+    path("search/", search_movie, name="search_movie"),
     path("movie/<slug:movie_slug>/", movie_detail, name="movie_detail"),
     path("actor/<slug:actor_slug>/", actor_detail, name="actor_detail"),
 ]

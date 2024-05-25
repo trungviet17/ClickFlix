b70a18e21482b83c5c228543707ba1ece54a9cab
Tuoc Nguyen <nguyenductuoc862@gmail.com>
2024-05-25 21:56:27 +0700

    update mymovie

--- myclickflix/main/urls.py
+++ myclickflix/main/urls.py
@@ -4,11 +4,12 @@ from main.views import (
     actor_detail,
     move_list,
     get_category,
+    your_movie_list,
 )
 
 
 urlpatterns = [
-    # path("category", get_category, name="category"),
+    path("mymovie/", your_movie_list, name="my_movie_list"),
     path("search/", move_list, name="search_movie"),
     path("movie/<slug:movie_slug>/", movie_detail, name="movie_detail"),
     path("actor/<slug:actor_slug>/", actor_detail, name="actor_detail"),
